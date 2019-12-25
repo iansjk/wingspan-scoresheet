83f06049e0b37135ddffdacccde983e50b0885f0
Ian Kim <ikim2@wcpschools.wcpss.local>
2019-12-25 18:09:11 -0500

    Improve visual layout

--- App.tsx
+++ App.tsx
@@ -1,83 +1,104 @@
-import React, { SyntheticEvent } from 'react';
-import { StyleSheet, Text, View, TextInput, TextInputChangeEventData, NativeSyntheticEvent } from 'react-native';
-import { ScreenOrientation, AppLoading } from 'expo';
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, TextInputProps, StyleProp, TextStyle, ViewProps, ViewStyle, TextProps, KeyboardAvoidingView, StatusBar } from 'react-native';
+import { AppLoading } from 'expo';
 import * as Font from 'expo-font';
 
 const styles = StyleSheet.create({
-  container: {
+  tableCell: {
     flex: 1,
-    backgroundColor: '#fff',
+    padding: 10,
+    borderLeftWidth: 1,
+    borderBottomWidth: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  labelCell: {
+    borderLeftWidth: 0,
+    alignItems: 'flex-start'
   },
   defaultText: {
     fontSize: 20,
     fontFamily: 'cardenio-modern'
+  },
+  defaultTextInput: {
+    textAlign: 'center'
   }
 });
 
-interface DefaultProps {
-  style?: object
-}
-
-class Container extends React.Component<DefaultProps> {
+class TableCell extends React.Component<ViewProps> {
   render() {
-    return <View style={[styles.container, this.props.style]}>{this.props.children}</View>
+    return <View style={[styles.tableCell, this.props.style] as StyleProp<ViewStyle>}>{this.props.children}</View>
   }
 }
 
-class WSText extends React.Component<DefaultProps> {
+class LabelCell extends React.Component<ViewProps> {
   render() {
-    return <Text style={[styles.defaultText, this.props.style]}>{this.props.children}</Text>
+    return <View style={[styles.tableCell, styles.labelCell, this.props.style] as StyleProp<ViewStyle>}>{this.props.children}</View>
   }
 }
 
-interface WSTextInputProps {
-  editable?: boolean,
-  defaultValue?: string,
-  value?: string,
-  name?: string,
-  onChangeText?(text: string): void
+class WSText extends React.Component<TextProps> {
+  render() {
+    return <Text style={[styles.defaultText, this.props.style] as StyleProp<TextStyle>}>{this.props.children}</Text>
+  }
 }
 
-class WSTextInput extends React.Component<WSTextInputProps & DefaultProps> {
+class WSTextInput extends React.Component<TextInputProps> {
   render() {
-    return <TextInput style={[styles.defaultText, this.props.style]} {...this.props} />
+    return (
+      <TextInput
+        style={[styles.defaultText, styles.defaultTextInput, this.props.style] as StyleProp<TextStyle>}
+        {...this.props}
+      />
+    );
   }
 }
 
 export class ScoreLabelColumn extends React.Component {
   render() {
     return (
-      <Container>
-        <View style={{
-          flex: 7,
-          alignItems: 'flex-start'
+      <View style={{ flex: 1 }}>
+        <LabelCell />
+        <LabelCell>
+          <WSText>Birds</WSText>
+        </LabelCell>
+        <LabelCell>
+          <WSText>Bonus cards</WSText>
+        </LabelCell>
+        <LabelCell>
+          <WSText>End-of-round goals</WSText>
+        </LabelCell>
+        <LabelCell>
+          <WSText>Eggs</WSText>
+        </LabelCell>
+        <LabelCell>
+          <WSText>Food on cards</WSText>
+        </LabelCell>
+        <LabelCell>
+          <WSText>Tucked cards</WSText>
+        </LabelCell>
+        <LabelCell style={{
+          borderBottomWidth: 0,
+          alignItems: 'center'
         }}>
-          <Container />
-          <Container>
-            <WSText>Birds</WSText>
-          </Container>
-          <Container>
-            <WSText>Bonus cards</WSText>
-          </Container>
-          <Container>
-            <WSText>End-of-round goals</WSText>
-          </Container>
-          <Container>
-            <WSText>Eggs</WSText>
-          </Container>
-          <Container>
-            <WSText>Food on cards</WSText>
-          </Container>
-          <Container>
-            <WSText>Tucked cards</WSText>
-          </Container>
-        </View>
-        <Container>
-          <WSText style={{ textTransform: 'uppercase' }}>Total</WSText>
-        </Container>
-      </Container >
+          <WSText style={{
+            textTransform: 'uppercase',
+            fontFamily: 'cardenio-modern-bold'
+          }}>Total</WSText>
+        </LabelCell>
+      </View>
+    );
+  }
+}
+
+class ScoreCell extends React.Component<TextInputProps> {
+  render() {
+    return (
+      <WSTextInput
+        onFocus={(e) => console.log(e)}
+        keyboardType="numeric"
+        {...this.props}
+      />
     );
   }
 }
@@ -114,34 +135,37 @@ class PlayerScoreCard extends React.Component<PlayerScoreCardProps, PlayerScoreC
 
   render() {
     return (
-      <Container>
-        <Container>
+      <View style={{flex: 1}}>
+        <TableCell>
           <WSTextInput defaultValue={"Player " + this.props.number} />
-        </Container>
+        </TableCell>
         {
           this.state.subscores.map((_, i) => {
             return (
-              <Container key={i}>
-                <WSTextInput
+              <TableCell key={i}>
+                <ScoreCell
                   onChangeText={(text) => this.handleChangeText(text, i)}
                   value={this.state.subscores[i].toString()}
                 />
-              </Container>
+              </TableCell>
             );
           })
         }
-        <Container>
+        <TableCell style={{
+          borderBottomWidth: 0
+        }}>
           <WSTextInput
             editable={false}
             value={this.state.total.toString()}
           />
-        </Container>
-      </Container>
+        </TableCell>
+      </View>
     );
   }
 }
 
 const NUMPLAYERS = 2;
+const SCREEN_PADDING = 10;
 
 export default class App extends React.Component {
   state = {
@@ -159,21 +183,27 @@ export default class App extends React.Component {
       );
     }
     return (
-      <Container style={{ flexDirection: 'row' }}>
+      <KeyboardAvoidingView
+        style={{
+          flex: 1,
+          flexDirection: 'row',
+          marginTop: StatusBar.currentHeight + SCREEN_PADDING,
+          margin: SCREEN_PADDING
+        }}
+        behavior='padding'
+      >
         <ScoreLabelColumn />
         {Array.from(Array(NUMPLAYERS).keys()).map((i) =>
           <PlayerScoreCard key={i} number={i + 1} />
         )}
-      </Container>
+      </KeyboardAvoidingView>
     );
   }
 }
 
-async function loadAssets() {
-  await Promise.all([
-    Font.loadAsync({
-      'cardenio-modern': require('./assets/fonts/CardenioModern-Regular.otf')
-    }),
-    ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT)
-  ]);
+function loadAssets() {
+  return Font.loadAsync({
+    'cardenio-modern': require('./assets/fonts/CardenioModern-Regular.otf'),
+    'cardenio-modern-bold': require('./assets/fonts/CardenioModern-Bold.otf')
+  });
 }
