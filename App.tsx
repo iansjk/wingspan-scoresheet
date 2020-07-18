3ca4617c41facd85cb14f634ef85de8646ac4475
Ian Kim <me@ian.kim>
2020-07-17 20:56:23 -0400

    Add remaining markup for modal

--- App.tsx
+++ App.tsx
@@ -2,10 +2,52 @@ import { FontAwesome } from '@expo/vector-icons';
 import { AppLoading } from 'expo';
 import * as ScreenOrientation from 'expo-screen-orientation';
 import * as Font from 'expo-font';
-import React from 'react';
-import { Keyboard, StatusBar, StyleProp, StyleSheet, Text, TextInput, TextInputProps, TextProps, TextStyle, TouchableHighlightProps, TouchableWithoutFeedback, TouchableWithoutFeedbackProps, View, ViewProps, ViewStyle, KeyboardAvoidingView, Platform, Modal } from 'react-native';
+import React, { useState } from 'react';
+import { Keyboard, StatusBar, StyleProp, StyleSheet, Text, TextInput, TextInputProps, TextProps, TextStyle, TouchableHighlightProps, TouchableWithoutFeedback, TouchableWithoutFeedbackProps, View, ViewProps, ViewStyle, KeyboardAvoidingView, Platform, Modal, Switch, TouchableHighlight } from 'react-native';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { LinearGradient } from 'expo-linear-gradient';
 
 const styles = StyleSheet.create({
+  openButton: {
+    backgroundColor: "#F194FF",
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  placeText: {
+    fontSize: 10
+  },
+  pointText: {
+    fontSize: 24
+  },
+  switchbar: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center'
+  },
+  cell: {
+    flex: 1,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderTopWidth: 1,
+    borderLeftWidth: 1,
+    borderRightWidth: 1,
+    borderColor: 'black'
+  },
+  column: {
+    flex: 1,
+    height: '80%',
+    margin: 10,
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderColor: 'black'
+  },
+  columns: {
+    flex: 9,
+    flexDirection: 'row-reverse'
+  },
   tableCell: {
     flex: 1,
     borderLeftWidth: 1,
@@ -43,6 +85,7 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1
   },
   modalView: {
+    flex: 1,
     margin: 20,
     backgroundColor: "white",
     borderRadius: 20,
@@ -365,11 +408,68 @@ class PlayerScoreCard extends React.Component<PlayerScoreCardProps> {
   }
 }
 
+const mostModePoints: number[][] = [
+  [4, 1, 0, 0],
+  [5, 2, 1, 0],
+  [6, 3, 2, 0],
+  [7, 4, 3, 0]
+];
+const eachModePoints: number[][] = Array(4).fill(
+  Array(6).fill(5).map((a, b) => a - b)
+); // [5, 4, 3, 2, 1, 0], 4 times
+
+const mostModeGradientColors = ['#64A03E', '#D9DDC7'];
+const eachModeGradientColors = ['#79BBC6', '#ECFFF8'];
+
+interface PointTextProps {
+  value: number
+}
+
+const PointText: React.StatelessComponent<PointTextProps> = (props) => {
+  return (
+    <View style={{
+      flex: 1,
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      justifyContent: 'center'
+    }}>
+      <Text style={styles.pointText}>{props.value}</Text>
+      <FontAwesome5 name="feather-alt" size={24} color="black" />
+    </View>
+  );
+};
+
+interface PlaceTextProps {
+  index: number
+}
+
+const PlaceText: React.StatelessComponent<PlaceTextProps> = (props) => {
+  let ordinal = "th";
+  if (props.index === 0) {
+    ordinal = "st";
+  } else if (props.index === 1) {
+    ordinal = "nd";
+  } else if (props.index === 2) {
+    ordinal = "rd";
+  }
+  const text = `${props.index + 1}${ordinal} Place`;
+  return (
+    <Text style={styles.placeText}>{text}</Text>
+  );
+}
+
 interface EndOfRoundGoalsModalProps {
   visible: boolean
 }
 
 const EndOfRoundGoalsModal: React.StatelessComponent<EndOfRoundGoalsModalProps> = (props) => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [goalsMode, setGoalsMode] = useState("most");
+  const trackColor = {false: mostModeGradientColors[0], true: eachModeGradientColors[0]}
+  const iosBackgroundColor = trackColor["false"];
+  const gradientColors = (goalsMode === "most") ? mostModeGradientColors : eachModeGradientColors;
+  const points = (goalsMode === "most") ? mostModePoints : eachModePoints;
+
   return (
     <Modal
       animationType="slide"
@@ -377,7 +477,77 @@ const EndOfRoundGoalsModal: React.StatelessComponent<EndOfRoundGoalsModalProps> 
       visible={props.visible}
     >
       <View style={styles.modalView}>
+        <View style={styles.switchbar}>
+          <Text>Most</Text>
+          <Switch
+            style={{
+              marginLeft: 15,
+              marginRight: 15
+            }}
+            trackColor={trackColor}
+            ios_backgroundColor={iosBackgroundColor}
+            onValueChange={() => setGoalsMode(goalsMode === "most" ? "each" : "most")}
+            value={goalsMode === "each"}
+          />
+          <Text>1 Each</Text>
+        </View>
+        <View style={styles.columns}>
+          {
+            points.map((column, roundIndex) => {
+              return (
+                <View style={styles.column} key={roundIndex}>
+                  <Text style={{
+                    position: 'absolute',
+                    top: '-10%'
+                  }}>
+                    Round {roundIndex + 1}
+                  </Text>
+                  <LinearGradient
+                    colors={gradientColors}
+                    style={{
+                      position: 'absolute',
+                      left: 0,
+                      right: 0,
+                      top: 0,
+                      height: '100%',
+                    }}
+                  />
+                  {
+                    column.map((cell, placeIndex) => {
+                      return (
+                        <TouchableHighlight
+                          style={styles.cell}
+                          underlayColor={'rgba(0,0,0,0.3)'}
+                          onPress={(e) => {
+                            alert(`Clicked on Round ${roundIndex + 1}, Place: ${placeIndex + 1}`);
+                          }}
+                        >
+                          <View>
+                            <PointText value={cell} />
+                            {
+                              (placeIndex < 3) &&
+                              <PlaceText index={placeIndex} />
+                            }
+                          </View>
+                        </TouchableHighlight>
+                      )
+                    })
+                  }
+                </View>
+              )
+            }
+            )
+          }
+        </View>
 
+        <TouchableHighlight
+          style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
+          onPress={() => {
+            setModalVisible(!modalVisible);
+          }}
+        >
+          <WSText>Hide Modal</WSText>
+        </TouchableHighlight>
       </View>
     </Modal>
   );
@@ -411,7 +581,7 @@ export default class App extends React.Component<{}, AppState> {
       automaScores: this._initializeAutomaScores(),
       paddingBottom: 0,
       orientation: 'PORTRAIT',
-      endOfRoundGoalsModalVisible: false
+      endOfRoundGoalsModalVisible: true
     }
   }
 
